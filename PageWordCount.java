e2042e351add9d762417cad85103b471c3fddeef
Reeshabh <rpa254@nyu.edu>
2015-10-08 20:37:24 -0400

    completed bulding inverted index file --> sorted according to word and doc id

--- PageWordCount.java
+++ PageWordCount.java
@@ -4,7 +4,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
@@ -16,7 +15,8 @@ public class PageWordCount {
 		
 		String[] words = instring.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
 		
-		System.out.println(Arrays.toString(words));
+		/* Put the list of words in a hashtable to get frequency of each word*/
+		//System.out.println(Arrays.toString(words));
 		Hashtable<String, Integer> word_freq = new Hashtable<String, Integer>();
 		for(int i = 0 ; i < words.length; i++){
 			if(word_freq.containsKey(words[i])){
@@ -27,27 +27,29 @@ public class PageWordCount {
 			}
 		}
 		
-		System.out.println(word_freq.toString());
+		/* This enumeration is being used to iterate through the hashtable and 
+		 * for writing its contents onto the intermediate inverted index file 
+		 * which will be sorted in the main using unix sort */
+		//System.out.println(word_freq.toString());
 		Enumeration<String> entries = word_freq.keys();
-		
-		try {
 			
+		/*writing onto the intermediate index file here*/
+		try{
 			File inter_index = new File("intermediate_non_aggregated.txt");
 			if(!inter_index.exists())
 			inter_index.createNewFile();
 			FileWriter fw = new FileWriter(inter_index.getName(), true);
 			BufferedWriter bw = new BufferedWriter(fw);
 			
-			while(entries.hasMoreElements()){	
+			while(entries.hasMoreElements()){
 				String key = entries.nextElement().toString();
-				bw.write(doc_id + " " + key + " "+ word_freq.get(key).toString()+"\n");
+				bw.write(key+" "+String.valueOf(doc_id)+" "+word_freq.get(key).toString()+"\n");
 			}
-			
 			bw.close();
-			
-		} catch (IOException e) {
+		}catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}	
+		
 	}
 }
--- Uncompress.java
+++ Uncompress.java
@@ -1,12 +1,14 @@
 package web.search.uncompressor;
 
 import java.io.BufferedReader;
+//import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+//import java.util.Collections;
 import java.util.Iterator;
 import java.util.zip.GZIPInputStream;
 import java.io.FileNotFoundException;
@@ -18,14 +20,16 @@ import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 public class Uncompress {
 	
 	public static void main(String args[]) throws IOException{
-		try {
-			
+		try {	
 			/* uncompressing a sample tar file */
 			TarArchiveInputStream myTar = new TarArchiveInputStream(new FileInputStream(new File("vol_0_99.tar")));
 			
 			/* get the second entry in the tar file*/
 			ArchiveEntry ignore_entry, data_entry;
 			ignore_entry = myTar.getNextEntry();
+			
+			/* while loop comes here to start looking at the next data file */
+			
 			data_entry = myTar.getNextEntry();
 			String fileName = data_entry.getName();
 			
@@ -45,17 +49,29 @@ public class Uncompress {
 			br.close();
 			Iterator<IndexEntry> index_it = indexArray.iterator();
 			
+			int temp_test = 0; //delete this right away
 			
 			/* uncompress gzip file*/
 			byte[] buffer = new byte[512];
 			GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(fileName));
 			FileOutputStream out = new FileOutputStream("file1.txt", false);
 			int len;
-			int now = 0;   			//controlling amount of file read
+			int now = 0;   									//controlling amount of file read
 			int total = index_it.next().content_length;		//should not exceed more than this
+
+			/*operating at data file level
+			 * Create an inverted index for every data file. For this unzip
+			 * data while in chunks of 512 bytes. Parse every page which is 
+			 * specified by the index file. Receive the string and use 
+			 * PageWordCount to write -> Word DocID Freq on the intermediate
+			 * inverted index file. Sort this file after the end of while loop
+			 * using unix sort and write it to the same file */
 			
-			int console_count = 0; //delete this 
-			while((len = gzis.read(buffer, 0, 512)) > 0 ){
+
+			int document_id = 0; 
+			while((len = gzis.read(buffer, 0, 512)) > 0 && temp_test < 3000){
+				
+				temp_test++;		//delete this right away
 				
 				int offset = 0;
 				
@@ -82,10 +98,20 @@ public class Uncompress {
 					
 					//call parser with the current file
 					PageParser pp = new PageParser("file1.txt");
-					System.out.println(++console_count + ")" + pp.getString());
+					System.out.println(++document_id + ")" + pp.getString());
 					
-					//write word count for the page in an intermediate file
-					PageWordCount.writeWordCount(pp.getString(), console_count);
+					//receive page  word_freq array list from PageWordCount
+					//and add it to the data array list. 
+					//PageWordCount.writeWordCount(pp.getString(), document_id);
+					
+					/* page_list = */
+					PageWordCount.writeWordCount(pp.getString(), document_id);
+					
+					/*
+					for(String page_entry : page_list){
+						data_list.add(page_entry);
+					}
+					*/
 					System.out.println();
 					
 					//truncate file
@@ -98,15 +124,23 @@ public class Uncompress {
 					
 					//initialize now to size of remaining buffer and total to next size
 					now = len - (total - now);
-					total = index_it.next().content_length;
+					
+					if(index_it.hasNext())
+						total = index_it.next().content_length;
 				}
 				else {
 					out.write(buffer, offset, len-offset);
 					now = now + len-offset;
 				}
-				
-				
-			}
+		
+			} //reading one data file ends here.
+
+			/* sort intermediate files via unix command. 
+			 * This is for sorting the words according to
+			 * the first field --> word and the second 
+			 * field --> doc_id. */
+			Process sorter = Runtime.getRuntime().exec("sort -k 1,1 -k 2,2n intermediate_non_aggregated.txt -o intermediate_non_aggregated.txt");
+			sorter.waitFor();
 			
 			/*close the resources*/
 			gzis.close(); 
@@ -114,6 +148,9 @@ public class Uncompress {
 			myTar.close();
 			
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
